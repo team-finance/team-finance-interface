9dd337239b8ce3544db039ef28038b6a2de57302
dhanraj <dhanraj@aequalisys.com>
2021-06-09 11:28:54 +0530

    explore table ui minor fixes

--- src/app/components/Explore/index.tsx
+++ src/app/components/Explore/index.tsx
@@ -1,6 +1,6 @@
 import "../../../assets/scss/explore.scss";
 import ExploreCard from "./card";
-import { Table, Form, Button } from "react-bootstrap";
+import { Table, Form, Button, ProgressBar } from "react-bootstrap";
 import Logo from "../../../assets/images/metamask.png";
 
 const Explore = () => {
@@ -27,20 +27,11 @@ const Explore = () => {
         </div>
       </div>
       <div className="explorecard-tablemain">
-        {/* <Row className="m-0 explorecard-table"> */}
-        {/* <Col className="m-0 p-0"> */}
-        {/* <Form className="explore-search">
-          <Form.Control
-            type="text"
-            placeholder="Search for a token by name/address/contract"
-          />
-        </Form> */}
         <div className={`input-search-container w-100`}>
           <div className="input-with-icon">
             <input
               type="text"
-              placeholder={`Search for a token by name/address/contract`}
-              // placeholder="Search for a token by name/address/contract"
+              placeholder="Search for a token by name/address/contract"
             />
             <i className="fa fa-search" aria-hidden="true" />
           </div>
@@ -48,7 +39,7 @@ const Explore = () => {
         {/* </Col> */}
         <Table borderless hover size="md" className="explore-table">
           <thead>
-            <tr>
+            <tr style={{ color: "#666666", fontWeight: 100 }}>
               <th>NAME</th>
               <th>BLOCKCHAIN</th>
               <th>LIQUIDITY LOCKED</th>
@@ -60,43 +51,48 @@ const Explore = () => {
           </thead>
           <tbody className="table-body">
             <tr>
-              <td
-                style={{
-                  backgroundColor: "orange",
-                  display: "flex",
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                }}
-              >
-                <img
-                  src={Logo}
-                  alt="logo"
-                  style={{
-                    height: "20px",
-                    width: "20px",
-                    borderRadius: "50%",
-                  }}
-                />
+              <td className="name-cell">
+                <img src={Logo} alt="logo" className="name-cell-logo" />
                 <div>
-                  <p
-                    style={{
-                      backgroundColor: "red",
-                      fontSize: "1rem",
-                      margin: 0,
-                    }}
-                  >
-                    Kin
-                  </p>
-                  <p style={{ fontSize: "0.8rem", margin: 0, padding: 0 }}>
-                    kin
-                  </p>
+                  <p className="name-cell-mainvalue">UNI</p>
+                  <p className="name-cell-subvalue">uniswap</p>
                 </div>
               </td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-              <td>Otto</td>
-              <td>@mdo</td>
+              <td>
+                <div className="blockchain-cell-container">
+                  <img src={Logo} alt="logo" className="blockchain-cell-icon" />
+                  <p className="blockchain-value">ETH</p>
+                </div>
+              </td>
+              <td>
+                <div className="liquidity-cell-container">
+                  <img src={Logo} alt="logo" className="liquidity-cell-logo" />
+                  <p className="liquidity-cell-value">ETH</p>
+                </div>
+              </td>
+              <td>
+                <div className="tokens-container">
+                  <img src={Logo} alt="logo" className="tokens-logo" />
+                  <p className="token-value">ETH</p>
+                </div>
+              </td>
+              <td>
+                <div className="value-container">
+                  <p className="value-container-value">ETH</p>
+                </div>
+              </td>
+              <td>
+                <div className="nextunlock-container">
+                  <p style={{ fontSize: "0.8rem", margin: 0, padding: 0 }}>
+                    2 Months
+                  </p>
+                  <ProgressBar
+                    variant="success"
+                    now={100}
+                    style={{ height: "5px", width: "75%" }}
+                  />
+                </div>
+              </td>
               <td>
                 <Button variant="light">View</Button>
               </td>
